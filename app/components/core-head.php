98ee060fc07dfd7a8ebadc42ac9cf218705861cb
Tim Brugman <tim@timbrugman.com>
2021-07-11 00:18:04 +0200

    Add favicon
    Close #11

--- app/components/core-head.php
+++ app/components/core-head.php
@@ -11,6 +11,11 @@
     <!-- title -->
     <title>WP-CLI GUI</title>
 
+    <!-- link icon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2">
+    <link rel="shortcut icon" href="/favicon.ico?v=2">
+
     <!-- link css -->
     <link rel="stylesheet" href="/assets/app.min.css">
 
